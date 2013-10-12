2959e45f7fe38b0f38aeecaf052ad505692f1e4b
Maciek Sakrejda <maciek@heroku.com>
2013-10-12 10:34:07 -0700

    Break out the pieces

---
+++ public/assets/javascripts/connect.js
@@ -0,0 +1,47 @@
+$(function() {
+
+    var conn;
+    var msg = $("#msg");
+    var log = $("#log");
+
+    function appendLog(msg) {
+        var d = log[0]
+        var doScroll = d.scrollTop == d.scrollHeight - d.clientHeight;
+        msg.appendTo(log)
+        if (doScroll) {
+            d.scrollTop = d.scrollHeight - d.clientHeight;
+        }
+    }
+
+    $("#form").submit(function() {
+        if (!conn) {
+            return false;
+        }
+        if (!msg.val()) {
+            return false;
+        }
+        conn.send(msg.val());
+        msg.val("");
+        return false
+    });
+
+    if (window["WebSocket"]) {
+        var loc = window.location, wsUrl;
+        if (loc.protocol === "https:") {
+            wsUrl = "wss:";
+        } else {
+            wsUrl = "ws:";
+        }
+        wsUrl += "//" + loc.host + loc.pathname + "connect";
+
+        conn = new WebSocket(wsUrl);
+        conn.onclose = function(evt) {
+            appendLog($("<div><b>Connection closed.</b></div>"))
+        }
+        conn.onmessage = function(evt) {
+            appendLog($("<div/>").text(evt.data))
+        }
+    } else {
+        appendLog($("<div><b>Your browser does not support WebSockets.</b></div>"))
+    }
+});
