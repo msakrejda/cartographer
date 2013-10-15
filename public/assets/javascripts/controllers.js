2555a36a535e0ffa9470a4b5566fcc9c63b7aa5a
Maciek Sakrejda <maciek@heroku.com>
2013-10-15 06:59:58 -0700

    Skip the directive; go with a hacky controller-driven mechanism for now

--- public/assets/javascripts/controllers.js
+++ public/assets/javascripts/controllers.js
@@ -38,12 +38,113 @@ angular.module('app').controller('QueryCtrl', ['$scope', 'QueryService', functio
 }]);
 
 angular.module('app').controller('ChartCtrl', [ '$scope', function ChartCtrl($scope) {
+    // TODO: a fair amount of this should probably be pushed down into a directive
+    $scope.registeredCharts = []
 
     $scope.selectedResult = null;
+    $scope.selectedChart = null;
 
     $scope.$on('resultSelected', function(event, queryResult) {
 	console.log("update selectedResult to: " + queryResult.id);
         $scope.selectedResult = queryResult;
+        if (queryResult) {
+            if (canReload(queryResult)) {
+                $scope.currChart.reload(queryResult);
+            } else {
+                // We assume that at least one chart will always be available
+                $scope.selectChart($scope.availableCharts()[0]);
+            }
+        }
     });
 
+    function canReload(queryResult) {
+        return $scope.selectedChart && $scope.selectedChart.accepts(queryResult) &&
+            typeof($scope.currChart.loadData) == 'function';
+    }
+
+    $scope.availableCharts = function() {
+        return $scope.registeredCharts.filter(function(chart) {
+            return $scope.selectedResult && chart.accepts($scope.selectedResult);
+        });
+    }
+
+    $scope.addChart = function(chartFn) {
+        $scope.registeredCharts.push(chartFn);
+    }
+
+    $scope.selectChart = function(chart) {
+        window.console.log("selected " + chart.chartName);
+        $scope.selectedChart = chart;
+        if ($scope.selectedResult) {
+            loadChart(chart, $scope.selectedResult);
+        }
+    }
+
+    // load the given chart
+    function loadChart(chartFn, result) {
+        // TODO: reload new query into old charts for efficiency; also
+        // cache multiple chart types for quick navigation of most
+        // recent items recent items
+	container = $('#chart-container')
+        if ($scope.currChart) {
+            if (typeof($scope.currChart.dispose) == 'function') {
+                $scope.currChart.dispose();
+            }
+            container.children().remove();
+        }
+
+        $scope.currChart = new chartFn(container, result);
+
+        window.console.log("Loaded " + chartFn.chartName +
+                           " with result for " + result.query);
+    }
+
+    $scope.addChart(Table);
+
 }]);
+
+function Table(target, queryResult) {
+    function zpad(numstr, len) {
+        return numstr.length == len ? numstr : '0' + zpad(numstr, len - 1);
+    }
+    function formatDate(d) {
+        return d.getFullYear() + '-' +
+            zpad((d.getMonth() + 1).toString(), 2) + '-' +
+            zpad((d.getDate()).toString(), 2) + ' ' +
+            zpad((d.getHours()).toString(), 2) + ':' +
+            zpad((d.getMinutes()).toString(), 2) + ':' +
+            zpad((d.getSeconds()).toString(), 2) + '.' +
+            zpad((d.getMilliseconds()).toString(), 3);
+    }
+
+    function massageColumns(queryResult) {
+        return queryResult.columns.map(function(column) {
+            var tableColumn = {
+                "sTitle": column.name
+            };
+            if (isNumeric(column)) {
+                tableColumn.sClass = 'right-align';
+                tableColumn.fnRender = function(obj) {
+                    var result = obj.aData[obj.iDataColumn];
+                    return result.toFixed(column.type == 'integer' ? 0 : 3);
+                }
+            } else if (isDate(column)) {
+                tableColumn.fnRender = function(obj) {
+                    return formatDate(new Date(obj.aData[obj.iDataColumn]));
+                }
+            }
+            return tableColumn;
+        });
+    }
+
+    table = $('<table class="display"></table>').appendTo(target);
+    table.dataTable({
+        "bDestroy": true,
+        "aaData": queryResult.data,
+        "aoColumns": massageColumns(queryResult)
+    });
+}
+Table.chartName = 'Table'
+Table.accepts = function(queryResult) {
+    return true;
+}
